a7ee78885d4684a8b94d90785c7e9fe7ed26a3ed
devksh930 <ksh930@icloud.com>
2023-04-23 02:15:12 +0900

    Initial commit

---
+++ src/main/java/me/devksh930/sock/SockApplication.java
@@ -0,0 +1,13 @@
+package me.devksh930.sock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/devksh930/sock/SockApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devksh930.sock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
